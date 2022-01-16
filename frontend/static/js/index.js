d846423430f90dfe5ffcaf4efd889985705893b0
Noor Latif <noor.crystal@gmail.com>
2022-01-16 21:29:01 +0100

    add router

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,1 +1,7 @@
-console.log("js is loaded")
+const router = async () => {
+    const routes = [
+        { path: "/", view: () => console.log("Viewing Dashboard.") },
+        { path: "/posts", view: () => console.log("Viewing Posts.") },
+        { path: "/settings", view: () => console.log("Viewing Settings.") },
+    ];
+};
